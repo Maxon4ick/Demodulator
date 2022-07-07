42acb9007d8879b3bce4df010ed893d30d3e1721
Maxon4ick <empety_line@mail.ru>
2022-07-07 16:39:23 +0300

    aded main

--- main.cpp
+++ main.cpp
@@ -9,6 +9,23 @@
 
 int main()
 {
+    /*Ам демодулятор*/
+    std::vector<ComplexInt> inAm= FileManager::ReadSignal<ComplexInt>("am_sound.dat");
+    std::vector<ComplexFloat> temp(inAm.size());
+    ComplexFloat *tempPtr = &temp[0];
+    ComplexInt *inPtr = &inAm[0];
+    FileManager::ConvertSignal(inPtr,inAm.size(),tempPtr);
+    float fsAm =8e3;
+    Demodulate *demAm = new DemodulatorAM;
+    std::vector<float> outAm = demAm->Demod(temp,fsAm);
+    FileManager::SaveSignal(outAm,"testAm.bin");
+
+    /* Фм демодулятор*/
+    std::vector<ComplexFloat> inFm = FileManager::ReadSignal<ComplexFloat>("file1EuropaPlus.bin");
+    Demodulate *dem = new DemodulatorFM;
+    float fs = 500*10e5;
+    std::vector<float> out = dem->Demod(inFm,fs);
+    FileManager::SaveSignal(out,"testFm.bin");
 
 
     return 0;
